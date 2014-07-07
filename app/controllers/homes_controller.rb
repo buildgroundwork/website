e42bdfd01337aea5edcdf33a1b34b944758664ce
Adam Milligan and Masha Rikhter <pair+adam+masha@buildgroundwork.com>
2014-07-07 17:48:09 -0400

    Enforce response format in homes controller

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,8 @@
 class HomesController < ApplicationController
+  respond_to :html
+
   def show
+    respond_with(1)
   end
 end
 
--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -3,10 +3,22 @@ require 'rails_helper'
 describe HomesController do
   describe "#show" do
     subject { -> { get :show, format: format } }
-    let(:format) { :html }
 
-    it "should render the show template" do
-      expect(subject.call).to render_template(:show)
+    context "with a request for HTML" do
+      let(:format) { :html }
+
+      it "should render the show template" do
+        expect(subject.call).to render_template(:show)
+      end
+    end
+
+    context "with a request for JSON" do
+      let(:format) { :json }
+
+      it "should return 406" do
+        subject.call
+        expect(response.status).to equal(406)
+      end
     end
   end
 end
